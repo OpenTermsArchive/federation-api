6c9b97303ff792f74883f72d12281dbf4f386d15
Nicolas Dupont <npg.dupont@gmail.com>
2023-11-29 17:40:43 +0100

    Be more explicit in case of error

--- src/middlewares/errors.js
+++ src/middlewares/errors.js
@@ -2,7 +2,7 @@ import logger from '../utils/logger.js';
 
 function errorsMiddleware(err, req, res, next) {
   logger.error(err.stack);
-  res.status(500).send('Something went wrong!');
+  res.status(500).json({ error: err.message });
 }
 
 export default errorsMiddleware;
