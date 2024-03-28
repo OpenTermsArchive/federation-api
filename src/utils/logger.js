dbf469ba3faa5d6c186fc48042b146f5591ce1cf
Nicolas Dupont <npg.dupont@gmail.com>
2024-03-28 10:23:26 +0100

    Make the logger silent is test env

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const { combine, timestamp, printf, colorize } = winston.format;
 
-const transports = [new winston.transports.Console()];
+const transports = [new winston.transports.Console({ silent: process.env.NODE_ENV === 'test' })];
 
 const logger = winston.createLogger({
   format: combine(
