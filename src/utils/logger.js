d925569c6fe7d8e696271a89801f6cd0a0fc3e92
Nicolas Dupont <npg.dupont@gmail.com>
2024-05-06 11:06:34 +0200

    Prefix environment variables with OTA scope

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -22,15 +22,15 @@ const logger = winston.createLogger({
 });
 
 if (config.get('@opentermsarchive/federated-api.logger.sendMailOnError')) {
-  if (process.env.SMTP_PASSWORD === undefined) {
-    logger.warn('Environment variable "SMTP_PASSWORD" was not found; log emails cannot be sent');
+  if (process.env.OTA_FEDERATED_API_SMTP_PASSWORD === undefined) {
+    logger.warn('Environment variable "OTA_FEDERATED_API_SMTP_PASSWORD" was not found; log emails cannot be sent');
   } else {
     transports.push(new winston.transports.Mail({
       to: config.get('@opentermsarchive/federated-api.logger.sendMailOnError.to'),
       from: config.get('@opentermsarchive/federated-api.logger.sendMailOnError.from'),
       host: config.get('@opentermsarchive/federated-api.logger.smtp.host'),
       username: config.get('@opentermsarchive/federated-api.logger.smtp.username'),
-      password: process.env.SMTP_PASSWORD,
+      password: process.env.OTA_FEDERATED_API_SMTP_PASSWORD,
       ssl: true,
       timeout: 30 * 1000,
       formatter: args => args[Object.getOwnPropertySymbols(args)[1]], // Returns the full error message, the same that is visible in the console. It is referenced in the argument object with a Symbol of which we do not have the reference to but we know it is the second one.
