711f506ed5b93651c43b64db8644af85bd981584
Nicolas Dupont <npg.dupont@gmail.com>
2024-05-06 11:07:59 +0200

    Load dotenv first, without requiring manual import

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,11 +1,8 @@
 import os from 'os';
 
 import config from 'config';
-import dotenv from 'dotenv';
 import winston from 'winston';
 import 'winston-mail';
-
-dotenv.config();
 
 const { combine, timestamp, printf, colorize } = winston.format;
 
